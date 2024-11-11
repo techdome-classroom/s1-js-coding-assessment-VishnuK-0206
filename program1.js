22eb64553b05d85a01d0bee796e030831bff4c4b
unknown <vishnu.kams@gmail.com>
2024-11-11 13:22:05 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -2,7 +2,13 @@ const getTotalIsles = function (grid) {
 
 
   // write your code here
-  
+  if (!grid || !grid.length || !grid[0].length) {
+    return 0;
+}
+
+const rows = grid.length;
+const cols = grid[0].length;
+let islandCount = 0;
 
 };
 
